028cfa4d7e283780ea4d60453665468b7cf699da
mikemeding <mikeymeding@gmail.com>
2013-11-13 10:34:36 -0500

    first commit

---
+++ Tree/tree.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   tree.h
+ * Author: mike
+ *
+ * Created on November 13, 2013, 10:29 AM
+ */
+
+#ifndef TREE_H
+#define	TREE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TREE_H */
+
