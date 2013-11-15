eaf3d3018cc88ad47bfe46dff6748c170acc0c5a
mikemeding <mikeymeding@gmail.com>
2013-11-14 22:01:13 -0500

    filled in a couple of functions -MM

--- Tree/BinaryTree.h
+++ Tree/BinaryTree.h
@@ -24,54 +24,76 @@ add(BinaryTree* T, BinaryNode *node, int left_or_right, BinaryTreeData data)
 extern "C" {
 #endif
 
+#include <stdbool.h>
 
-    // This is for an individual node, containing node data and pointer to left and right children.
+   // This is for an individual node, containing node data and pointer to left and right children.
 
-    typedef struct treenode {
-        NodeData data;
-        struct treenode *left, *right;
-    } TreeNode, *TreeNodePtr;
+   typedef struct node {
+	  NodeData data;
+	  struct node *left, *right;
+   } BinaryNode, *BinaryNodePtr;
 
-    // This structure is the root node, the very top level.
+   // This structure is the root node, the very top level.
 
-    typedef struct {
-        TreeNodePtr root;
-        TreeNodePtr current;
-    } BinaryTree;
+   typedef struct tree {
+	  BinaryNodePtr root;
+	  BinaryNodePtr current;
+   } BinaryTree, *Tree;
 
-    /*PROTOTYPES*/
-//    hasLeft(BinaryNode *node);
-//    hasRight(BinaryNode *node);
-//    isLeaf(BinaryNode *node);
-//    initBinaryTree(), (initializes Binary Tree);
-//    fill_root(BinaryTree* T, BinaryTreeData data);
-//    add(BinaryTree* T, BinaryNode *node, int left_or_right, BinaryTreeData data);
-//
-//    /*FUNCTIONS*/
-//
-//    hasLeft(BinaryNode *node) {
-//
-//    }
-//
-//    hasRight(BinaryNode *node) {
-//
-//    }
-//
-//    isLeaf(BinaryNode *node) {
-//
-//    }
-//
-//    initBinaryTree(), (initializes Binary Tree) {
-//
-//    }
-//
-//    fill_root(BinaryTree* T, BinaryTreeData data) {
-//
-//    }
-//
-//    add(BinaryTree* T, BinaryNode *node, int left_or_right, BinaryTreeData data) {
-//
-//    }
+   /*PROTOTYPES*/
+   bool hasLeft(BinaryNodePtr);
+   bool hasRight(BinaryNodePtr);
+   bool isLeaf(BinaryNodePtr);
+   Tree initBinaryTree(void);
+   void fill_root(Tree, BinaryNodePtr);
+   void add(Tree, BinaryNodePtr, int);
+
+   /*FUNCTIONS*/
+
+   /*Initalizes a new binary tree*/
+   Tree initBinaryTree() {
+	  Tree T = (Tree) malloc(sizeof (BinaryTree)); // allocate new space in memory for Tree
+	  T->root = NULL;
+	  T->current = NULL;
+	  return T;
+   }
+
+   /*Return true/false if the node at the address has a left value*/
+   bool hasLeft(BinaryNodePtr node) {
+	  if (node->left == NULL) {
+		 return false;
+	  } else {
+		 return true;
+	  }
+   }
+
+   /*Return true/false if the node at the address has a right value*/
+   bool hasRight(BinaryNodePtr node) {
+	  if (node->right == NULL) {
+		 return false;
+	  } else {
+		 return true;
+	  }
+   }
+
+   /*Return true/false if the node at the address is an outer leaf*/
+   bool isLeaf(BinaryNodePtr node) {
+	  if (!hasRight(node)&&!hasLeft(node)) {
+		 return true; // if node has no valid pointers to new values it is considered a leaf
+	  } else {
+		 return false; // if current node has more right or left values
+	  }
+   }
+
+   /*Fill the root value with the values given*/
+   void fill_root(Tree T, BinaryNodePtr node) {
+
+   }
+
+   /*Add a leaf to a give position in the tree*/
+   void add(Tree T, BinaryNodePtr node, int left_or_right) {
+
+   }
 
 
 
