3761cab903bd55fd19d9e3ca71f99e41e6bb4c7b
mikemeding <mikeymeding@gmail.com>
2013-11-18 10:07:17 -0500

    added some functions to library -MM

--- Tree/BinaryTree.h
+++ Tree/BinaryTree.h
@@ -25,75 +25,97 @@ extern "C" {
 #endif
 
 #include <stdbool.h>
+#define max(a,b) ((a)>(b)?(a):(b))
 
-   // This is for an individual node, containing node data and pointer to left and right children.
+    // This is for an individual node, containing node data and pointer to left and right children.
 
-   typedef struct node {
-	  NodeData data;
-	  struct node *left, *right;
-   } BinaryNode, *BinaryNodePtr;
+    typedef struct node {
+        NodeData data;
+        struct node *left, *right;
+    } BinaryNode, *BinaryNodePtr;
 
-   // This structure is the root node, the very top level.
+    // This structure is the root node, the very top level.
 
-   typedef struct tree {
-	  BinaryNodePtr root;
-	  BinaryNodePtr current;
-   } BinaryTree, *Tree;
+    typedef struct tree {
+        BinaryNodePtr root;
+        BinaryNodePtr current;
+    } BinaryTree, *Tree;
 
-   /*PROTOTYPES*/
-   bool hasLeft(BinaryNodePtr);
-   bool hasRight(BinaryNodePtr);
-   bool isLeaf(BinaryNodePtr);
-   Tree initBinaryTree(void);
-   void fill_root(Tree, BinaryNodePtr);
-   void add(Tree, BinaryNodePtr, int);
+    /*PROTOTYPES*/
+    bool hasLeft(BinaryNodePtr);
+    bool hasRight(BinaryNodePtr);
+    bool isLeaf(BinaryNodePtr);
+    Tree initBinaryTree(void);
+    void fill_root(Tree, BinaryNodePtr);
+    void add(Tree, BinaryNodePtr, int);
+    int height(Tree T);
+    int numNodes(Tree T);
+    int numLeaves(Tree T);
 
-   /*FUNCTIONS*/
+    /*FUNCTIONS*/
 
-   /*Initalizes a new binary tree*/
-   Tree initBinaryTree() {
-	  Tree T = (Tree) malloc(sizeof (BinaryTree)); // allocate new space in memory for Tree
-	  T->root = NULL;
-	  T->current = NULL;
-	  return T;
-   }
+    /*Initalizes a new binary tree*/
+    Tree initBinaryTree() {
+        Tree T = (Tree) malloc(sizeof (BinaryTree)); // allocate new space in memory for Tree
+        T->root = NULL;
+        T->current = NULL;
+        return T;
+    }
 
-   /*Return true/false if the node at the address has a left value*/
-   bool hasLeft(BinaryNodePtr node) {
-	  if (node->left == NULL) {
-		 return false;
-	  } else {
-		 return true;
-	  }
-   }
+    /*Return true/false if the node at the address has a left value*/
+    bool hasLeft(BinaryNodePtr node) {
+        if (node->left == NULL) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-   /*Return true/false if the node at the address has a right value*/
-   bool hasRight(BinaryNodePtr node) {
-	  if (node->right == NULL) {
-		 return false;
-	  } else {
-		 return true;
-	  }
-   }
+    /*Return true/false if the node at the address has a right value*/
+    bool hasRight(BinaryNodePtr node) {
+        if (node->right == NULL) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-   /*Return true/false if the node at the address is an outer leaf*/
-   bool isLeaf(BinaryNodePtr node) {
-	  if (!hasRight(node)&&!hasLeft(node)) {
-		 return true; // if node has no valid pointers to new values it is considered a leaf
-	  } else {
-		 return false; // if current node has more right or left values
-	  }
-   }
+    /*Return true/false if the node at the address is an outer leaf*/
+    bool isLeaf(BinaryNodePtr node) {
+        if (!hasRight(node)&&!hasLeft(node)) {
+            return true; // if node has no valid pointers to new values it is considered a leaf
+        } else {
+            return false; // if current node has more right or left values
+        }
+    }
 
-   /*Fill the root value with the values given*/
-   void fill_root(Tree T, BinaryNodePtr node) {
+    /*Fill the root value with the values given*/
+    void fill_root(Tree T, BinaryNodePtr node) {
 
-   }
+    }
 
-   /*Add a leaf to a give position in the tree*/
-   void add(Tree T, BinaryNodePtr node, int left_or_right) {
+    /*Add a leaf to a give position in the tree*/
+    void add(Tree T, BinaryNodePtr node, int left_or_right) {
 
-   }
+    }
+
+    int height(Tree T) {
+        if (T->current == NULL) return 0;
+        return 1 + max(height(T->root->left) + height(T->root->right));
+
+    }
+
+    int numNodes(Tree T) {
+        if (T->current == NULL)return 0;
+        return 1 + numNodes(T->root->left) + numNodes(T->root->right);
+
+    }
+
+    int numLeaves(Tree T) {
+        if (T->current == NULL)return 0;
+        if (T->root->left == NULL && T->root->right == NULL) return 1;
+        return numLeaves(T->root->left)+(T->root->right);
+    }
 
 
 
